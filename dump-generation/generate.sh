75727bcbf7c21ced3d554fca8d5ddbf72e00308f
Daniele Santini <danysan95@gmail.com>
2026-05-02 16:52:08 +0200

    Fix generation

--- dump-generation/generate.sh
+++ dump-generation/generate.sh
@@ -1,16 +1,16 @@
 #!/bin/bash
 
+SOURCE_DUMP='/public/dumps/public/wikidatawiki/entities/latest-all.json.gz'
+AVAILABLE_DUMP_DATE=$(date -r $SOURCE_DUMP "+%Y-%m-%d")
 if [[ " $* " == *" --test "* ]] ; then
+    TEST_MODE=true
     set -e
     set -x
-    TEST_MODE=true
+    PLACES_JSON_PATH="./$AVAILABLE_DUMP_DATE/places.test.ndjson"
 else
     TEST_MODE=false
+    PLACES_JSON_PATH="./$AVAILABLE_DUMP_DATE/places.ndjson"
 fi
-SOURCE_DUMP='/public/dumps/public/wikidatawiki/entities/latest-all.json.gz'
-
-available_dump_date=$(date -r $SOURCE_DUMP "+%Y-%m-%d")
-places_json_path="./$available_dump_date/places.ndjson"
 
 declare -a filter_options
 filter_options+=(--simplify --omit aliases --claim 'P625&~P585&~P376&~P580&~P571&~P1619&~P582&~P576&~P3999')
@@ -21,14 +21,15 @@ filter_options+=(--simplify --omit aliases --claim 'P625&~P585&~P376&~P580&~P571
 #TODO Allow P580, P571, P1619 (start dates) with values in the past
 #TODO Allow P582, P576, P3999 (end dates) with values in the future
 
-if [ -f $places_json_path ]; then
-    echo "$places_json_path already exists"
+mkdir -p "./$AVAILABLE_DUMP_DATE"
+if [ -f $PLACES_JSON_PATH ]; then
+    echo "$PLACES_JSON_PATH already exists"
 elif $TEST_MODE ; then
-    echo "Filtering $places_json_path from only the first 10'000 lines"
-    time cat $SOURCE_DUMP | gzip -d | head -10000 | cat - <(echo ']') | grep 'P625":' | wikibase-dump-filter "${curl_options[@]}" > $places_json_path
+    echo "Filtering $PLACES_JSON_PATH from only the first 10'000 lines"
+    time cat $SOURCE_DUMP | gzip -d | head -10000 | cat - <(echo ']') | grep 'P625":' | wikibase-dump-filter "${curl_options[@]}" > $PLACES_JSON_PATH
 else
-    echo "Filtering $places_json_path"
-    #cat $SOURCE_DUMP | gzip -d | grep 'P625":' | wikibase-dump-filter "${curl_options[@]}" > $places_json_path
+    echo "Filtering $PLACES_JSON_PATH"
+    #cat $SOURCE_DUMP | gzip -d | grep 'P625":' | wikibase-dump-filter "${curl_options[@]}" > $PLACES_JSON_PATH
     #TODO uncomment when implementation complete
 fi
 
