663050a18f8666c0918f202f65141f30f391c480
Daniele Santini <danysan95@gmail.com>
2026-05-02 13:08:20 +0200

    WIP filter_options

--- dump-generation/generate.sh
+++ dump-generation/generate.sh
@@ -11,9 +11,16 @@ SOURCE_DUMP='/public/dumps/public/wikidatawiki/entities/latest-all.json.gz'
 
 available_dump_date=$(date -r $SOURCE_DUMP "+%Y-%m-%d")
 places_json_path="./$available_dump_date/places.ndjson"
+
 declare -a filter_options
-filter_options+=(--claim P625)
-# TODO other options
+filter_options+=(--simplify --claim 'P625&~P585&~P376&~P580&~P571&~P1619&~P582&~P576&~P3999')
+# P625 (coordinates) must be present
+# P585 (date) or P376 (located on astronomical body) must be absent
+#TODO Allow P3896 (geoshape) alternatively to P625
+#TODO Check that no P582 qualifier is present in the P625 claim
+#TODO Allow P580, P571, P1619 (start dates) with values in the past
+#TODO Allow P582, P576, P3999 (end dates) with values in the future
+
 if [ -f $places_json_path ]; then
     echo "$places_json_path already exists"
 elif $TEST_MODE ; then
