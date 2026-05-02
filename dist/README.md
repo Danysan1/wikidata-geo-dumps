cdf0f95ce9cd3efe2ce5f14ee81819d242eea7ba
Daniele Santini <danysan95@gmail.com>
2026-05-02 10:19:13 +0200

    WIP Procfile

---
+++ dist/README.md
@@ -0,0 +1,1 @@
+This folder contains the static code deployed to the web app
---
+++ update-web-service.sh
@@ -0,0 +1,5 @@
+#!/bin/sh
+
+toolforge build start https://gitlab.wikimedia.org/toolforge-repos/wikidata-geo-dumps.git
+toolforge build show
+toolforge webservice buildservice start --mount=none 
