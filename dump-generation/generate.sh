56a171fc20711d7d4006f0ad9162fc1195a2f435
Daniele Santini <danysan95@gmail.com>
2026-05-06 14:07:34 +0200

    Optimize dump generation

--- dump-generation/generate.sh
+++ dump-generation/generate.sh
@@ -2,7 +2,6 @@
 
 #region Configs
 SOURCE_DUMP='/public/dumps/public/wikidatawiki/entities/latest-all.json.gz'
-SPLIT_COMPLEX_ITEMS=false # Using the same filters and 1M source dump lines, splitting complex items reduces the filtering time from 9m37s to 7m9s
 LANGUAGES=en # TODO use
 PROPERTIES=P31 # TODO use
 #endregion
@@ -31,30 +30,26 @@ TMP_DIR=$(mktemp -d)
 #endregion
 
 #region Filter and convert to GeoJSONSeq
-COMPLEX_GREP_FILTER='P585":|P376":|P580":|P571":|P1619":|P582":|P576":|P3999":'
-COMPLEX_ITEMS_PATH="$TMP_DIR/complex.ndjson"
-
-# Options documentation in https://codeberg.org/maxlath/wikibase-dump-filter/src/branch/main/docs/cli.md
-declare -a filter_options
-filter_options+=(--omit aliases,descriptions,sitelinks --languages mul,$LANGUAGES --claim 'P625&~P585&~P376&~P580&~P571&~P1619&~P582&~P576&~P3999')
-# P625 (coordinates) must be present
-# P585 (date) or P376 (located on astronomical body) must be absent
+# Wikidata JSON dump format: https://doc.wikimedia.org/Wikibase/master/php/docs_topics_json.html
+# Each output line is a single GeoJSON Feature (RFC 8142 newline-delimited).
+# Selection rules:
+#   P625 (coordinates) must be present
+#   P585 (date), P376 (located on astronomical body), P580/P571/P1619 (start dates),
+#   P582/P576/P3999 (end dates) must all be absent
 #TODO Allow P3896 (geoshape) alternatively to P625
 #TODO Check that no P582 qualifier is present in the P625 claim
 #TODO Allow P580, P571, P1619 (start dates) with values in the past
 #TODO Allow P582, P576, P3999 (end dates) with values in the future
-
-# Each output line is a single GeoJSON Feature (RFC 8142 newline-delimited).
-# The filter:
-#   - skips lines that are not JSON objects
-#   - emits one Feature for the first P625 statement with a valid globecoordinate value
-#   - rejects statements where latitude/longitude are not numbers
 JQ_FILTER='
     try fromjson catch empty
     | select(type == "object")
+    | select(.claims.P625)
+    | select(.claims.P585 == null and .claims.P376 == null
+             and .claims.P580 == null and .claims.P571 == null
+             and .claims.P1619 == null and .claims.P582 == null
+             and .claims.P576 == null and .claims.P3999 == null)
     | . as $item
-    | (.claims.P625 // [])[0]
-    | select(. != null)
+    | .claims.P625[0]
     | select(
         .mainsnak.snaktype == "value"
         and (.mainsnak.datavalue.value | type) == "object"
@@ -67,7 +62,7 @@ JQ_FILTER='
         properties: {
             id: $item.id,
             "name": $item.labels.mul.value,
-            "name:en": $item.labels.en.value,
+            "name:en": $item.labels.en.value
         },
         geometry: {
             type: "Point",
@@ -81,12 +76,11 @@ if [ -f "$PLACES_GEOJSONSEQ_PATH" ]; then
     echo "$PLACES_GEOJSONSEQ_PATH already exists"
 else
     echo "Filtering $PLACES_GEOJSONSEQ_PATH from $SOURCE_DUMP"
-    if $SPLIT_COMPLEX_ITEMS ; then
-        time gzip -d < "$SOURCE_DUMP" | ($TEST_MODE && head -1000000 || cat -) | grep 'P625":' | tee >(grep -E $COMPLEX_GREP_FILTER > "$COMPLEX_ITEMS_PATH") | grep -Ev $COMPLEX_GREP_FILTER | jq --raw-input -c "$JQ_FILTER" > "$PLACES_GEOJSONSEQ_PATH"
-        time wikibase-dump-filter "${filter_options[@]}" < "$COMPLEX_ITEMS_PATH" | jq --raw-input -c "$JQ_FILTER" >> "$PLACES_GEOJSONSEQ_PATH"
-    else
-        time gzip -d < "$SOURCE_DUMP" | ($TEST_MODE && head -1000000 || cat -) | grep 'P625":' | wikibase-dump-filter "${filter_options[@]}" | jq --raw-input -c "$JQ_FILTER" >> "$PLACES_GEOJSONSEQ_PATH"
-    fi
+    time pigz -dc "$SOURCE_DUMP" \
+        | ($TEST_MODE && head -1000000 || cat -) \
+        | grep 'P625":' \
+        | parallel --pipe --block 10M -j 0 --keep-order=0 jq --raw-input -c "$JQ_FILTER" \
+        > "$PLACES_GEOJSONSEQ_PATH"
 fi
 #endregion
 
